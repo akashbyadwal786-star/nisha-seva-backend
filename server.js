2de3fb40b3220f40492fa5a4e22c98cb2cbe6baa
akashbyadwal786-star <akashbyadwal786@gmail.com>
2026-09-26 17:04:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -446,7 +446,10 @@ app.get('/', (req, res) => {
 
 
 /* ---------- Start Server ---------- */
-
+submissionRoutes('volunteers');
+submissionRoutes('members');
+submissionRoutes('contactMessages');
+submissionRoutes('newsletter');
 app.listen(PORT, () => {
   console.log(
     `Nisha Seva backend running on port ${PORT}`
